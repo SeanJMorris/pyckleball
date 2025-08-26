999aafc7adc25e1be664e9b80c19de648c422055
Sean Morris <seanj.morris@gmail.com>
2025-08-25 20:30:52 -0400

    Hoping to sign up for a session at 1:15 UTC, 9:15 ET.

--- hq.py
+++ hq.py
@@ -6,15 +6,17 @@ from date_variables import day_next_sign_up_opp_24_hours, sign_up_today_for_sess
 if __name__ == "__main__":
 
     # case_day = datetime(2025, 8, 25, 21, 45)
-    case_day = day_next_sign_up_opp_24_hours()
+    # case_day = day_next_sign_up_opp_24_hours()
     headless = True
-    sign_up_moment = sign_up_today_for_session_in_24_hours()
+    # sign_up_moment = sign_up_today_for_session_in_24_hours()
 
     # sign_up_moment = datetime(2025, 8, 24, 21, 45)
 
     # case_day = datetime(2025, 8, 25, 9, 15)
     # headless = True
     # sign_up_moment = datetime.now() + timedelta(seconds=30)
+    case_day = datetime(2024, 8, 26, 21, 45)
+    sign_up_moment = case_day - timedelta(days=1)
 
     case_register(case_day, "registrant", headless, sign_up_moment)
     # case_deregister(case_day, "registrant", headless)
--- hq2.py
+++ hq2.py
@@ -5,7 +5,8 @@ from date_variables import day_next_sign_up_opp_24_hours
 if __name__ == "__main__":
 
     # case_day = datetime(2025, 8, 25, 21, 45)
-    case_day = day_next_sign_up_opp_24_hours()
+    # case_day = day_next_sign_up_opp_24_hours()
+    case_day = datetime(2024, 8, 26, 21, 45)
     headless = True
     sign_up_24_hr_advance = True
 
