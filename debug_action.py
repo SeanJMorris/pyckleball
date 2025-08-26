c52b967c6b59e709c5edce30a2dbb14653ad66c2
Sean Morris <seanj.morris@gmail.com>
2025-08-26 15:30:00 -0400

    Update debug workflow schedule and enhance debug_action.py logging

--- debug_action.py
+++ debug_action.py
@@ -17,8 +17,8 @@ def datetime_formatter(datetime_input: datetime) -> str:
     date_string_to_print = datetime_input.strftime('%a, %d %b %Y %I:%M:%S %Z')
     return date_string_to_print
 
-print_blue(f"case_day_ny_tz:--------------------------------{datetime_formatter(case_day_ny_tz)}")
 print_blue(f"case_day_utc:----------------------------------{datetime_formatter(case_day_utc)}")
+print_blue(f"case_day_ny_tz:--------------------------------{datetime_formatter(case_day_ny_tz)}")
 
 
 case_day_utc_for_yml = case_day_utc.strftime('%a, %b %-d @ %-I:%M')
@@ -27,7 +27,7 @@ print_blue("Chron format...................................min hr day_of_month m
 print_blue("Chron format...................................m_ h_ d m d")
 print_blue(f"Right now UTC time in cron format:-------------{my_utc_string_cron}")
 
-moment_to_run_yml_file_ny_tz = datetime(2025, 8, 26, 16, 0).astimezone(ny_tz)
-moment_to_run_yml_file_utc = moment_to_run_yml_file_ny_tz.astimezone(utc)
-print_blue(f"I'm expecting this script to run at nytz: {datetime_formatter(moment_to_run_yml_file_ny_tz)}")
-print_blue(f"I'm expecting this script to run at utc: {datetime_formatter(moment_to_run_yml_file_utc)}")
+# moment_to_run_yml_file_ny_tz = datetime(2025, 8, 26, 16, 0).astimezone(ny_tz)
+# moment_to_run_yml_file_utc = moment_to_run_yml_file_ny_tz.astimezone(utc)
+# print_blue(f"I'm expecting this script to run at nytz: {datetime_formatter(moment_to_run_yml_file_ny_tz)}")
+# print_blue(f"I'm expecting this script to run at utc: {datetime_formatter(moment_to_run_yml_file_utc)}")
