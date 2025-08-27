be8eaf567f04bf21b77ab6298fb8bc7a444af285
Sean Morris <seanj.morris@gmail.com>
2025-08-27 18:11:39 -0400

    Attempt to clean up case_create.py so that program can know that user was added successfully.

--- case_register.py
+++ case_register.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from playwright.sync_api import expect
+from playwright.sync_api import Page, expect
 import time
 
 from utilities.logger import logger
@@ -11,6 +11,25 @@ from datetime import datetime, timedelta
 from pytz import timezone
 from wait_until_sign_up_moment import wait_until_sign_up_moment
 from screenshots.screenshot_util import take_screenshot
+
+
+def access_session_to_sign_up(page: Page, user_type, case_day_strings):
+    if user_type == "pro":
+        page.get_by_role("button", name=case_day_strings["for_registering_list_mode"]).first.click()
+    elif user_type == "registrant":
+        page.get_by_role("button", name=case_day_strings['for_registering_bubble_mode']).first.click()
+
+def validate_user_added(page: Page, user_type: str) -> bool:
+    if user_type == "pro":
+        is_registered = "Sean Morris (4.0)" in page.get_by_role("rowgroup").inner_text()
+    elif user_type == "registrant":
+        is_registered = "Mobley Hesperweld (4.0)" in page.get_by_role("rowgroup").inner_text()
+    # return is_registered
+    # time.sleep(1)
+    if is_registered:
+        logger.debug(f"SUCCESS: User was added to the session.")
+    else:
+        logger.error(f"FAILURE: User was NOT added to the session.")
 
 def case_register(case_day_input: datetime,
                   user_type: str,
@@ -34,35 +53,28 @@ def case_register(case_day_input: datetime,
         # page.screenshot(path=f"screenshots/{datetime.now()}.png")
         # take_screenshot(page)
 
-        if user_type == "pro":
-            page.get_by_role("button", name=case_day_strings["for_registering_list_mode"]).first.click()
-        elif user_type == "registrant":
-            page.get_by_role("button", name=case_day_strings['for_registering_bubble_mode']).first.click()
-
-        # if yes_pause: page.pause()
-
-        page.get_by_role("button", name="+ Add My Name").click()
+        access_session_to_sign_up(page, user_type, case_day_strings)
 
         if yes_pause: page.pause()
 
-        time.sleep(1)
+        page.get_by_role("button", name="+ Add My Name").click()
 
-        if user_type == "pro":
-            is_registered = "Sean Morris (4.0)" in page.get_by_role("rowgroup").inner_text()
-        elif user_type == "registrant":
-            is_registered = "Mobley Hesperweld (4.0)" in page.get_by_role("rowgroup").inner_text()
+        # if yes_pause: page.pause()
 
-        time.sleep(1)
-        if is_registered:
-            logger.debug(f"SUCCESS: User was added to the session on {notification_input}.")
-        else:
-            raise Exception(f"FAILURE: User was NOT added to the session on {notification_input}.")
+        # time.sleep(1)
+        # page.get_by_role("rowgroup").wait_for(timeout=5000)
+        # logger.debug(f"Rowgroup inner text: " + page.get_by_role("rowgroup").inner_text())
+
+        #Verify user has been added by closing the session and opening it again
+        page.get_by_role("button", name="Close").click()
+        access_session_to_sign_up(page, user_type, case_day_strings)
+        validate_user_added(page, user_type)
 
         # take_screenshot(page)
 
 if __name__ == "__main__":
     ny_timezone = timezone("America/New_York")
-    case_day = ny_timezone.localize(datetime(2025, 8, 27, 19, 45))
+    case_day = ny_timezone.localize(datetime(2025, 8, 27, 17, 15))
     user_type = "registrant"
     headless = True
     case_register(case_day, user_type, headless)
--- hq.py
+++ hq.py
@@ -9,16 +9,17 @@ from utilities.logger import logger
 if __name__ == "__main__":
 
     ny_timezone = timezone("America/New_York")
-    case_day = ny_timezone.localize(datetime(2025, 8, 28, 17, 15))
+    # case_day = ny_timezone.localize(datetime(2025, 8, 28, 17, 15))
+    case_day = ny_timezone.localize(datetime(2025, 8, 27, 17, 15))
     user_type = "registrant"
     headless = True
-    sign_up_moment = case_day - timedelta(days=1)
+    # sign_up_moment = case_day - timedelta(days=1)
 
     logger.info(f"case_day          {case_day}")
-    logger.info(f"sign_up_moment:   {sign_up_moment}")
+    # logger.info(f"sign_up_moment:   {sign_up_moment}")
 
     # wait_until_sign_up_moment(sign_up_moment)
     case_register(case_day_input = case_day,
                   user_type = user_type,
-                  headless = headless,
-                  sign_up_moment = sign_up_moment)
+                  headless = headless)
+                #   sign_up_moment = sign_up_moment)
